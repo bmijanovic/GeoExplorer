01f3de137e6ee15a9384645b63d57bd809432d93
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2023-04-08 20:14:00 +0200

    Added Design For All Countries Page

--- geo-explorer/src/app/modules/countries-list/countries-list.module.ts
+++ geo-explorer/src/app/modules/countries-list/countries-list.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CountriesListComponent } from './page/countries-list.component';
 import {SharedModule} from "../shared/shared.module";
+import {MaterialModule} from "../../../infrastructure/material.module";
+import {ReactiveFormsModule} from "@angular/forms";
+import { CountryCardComponent } from './components/country-card/country-card.component';
+import {MatButtonModule} from "@angular/material/button";
 
 
 
 @NgModule({
   declarations: [
-    CountriesListComponent
+    CountriesListComponent,
+    CountryCardComponent
   ],
-  imports: [
-    CommonModule,
-    SharedModule
-  ]
+    imports: [
+        CommonModule,
+        SharedModule,
+        MaterialModule,
+        ReactiveFormsModule,
+        MatButtonModule
+    ]
 })
 export class CountriesListModule { }
--- geo-explorer/src/app/modules/countries-list/page/countries-list.component.ts
+++ geo-explorer/src/app/modules/countries-list/page/countries-list.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from "@angular/forms";
+import {animate, AUTO_STYLE, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-countries-list',
   templateUrl: './countries-list.component.html',
-  styleUrls: ['./countries-list.component.scss']
+  styleUrls: ['./countries-list.component.scss'],
+  animations: [
+    trigger('collapse', [
+      state('false', style({ height: AUTO_STYLE, visibility: AUTO_STYLE })),
+      state('true', style({ height: '0', visibility: 'hidden' })),
+      transition('false => true', animate(300 + 'ms ease-in')),
+      transition('true => false', animate(300 + 'ms ease-out'))
+    ])
+  ]
 })
-export class CountriesListComponent {
+export class CountriesListComponent implements  OnInit{
 
+  searchForm: any;
+  sortDirection="arrow_upward";
+  criteriums=["Date","Population"];
+  advancedSearchIcon="tune"
+  collapsed=true
+  icon:any;
+  continents= ["Europe","Asia","North America","South America","Africa","Antartica"];
+  continentsSelected=new FormControl("");
+  isIndepentent= false;
+  checkboxControl = new FormControl(false);
+
+
+  changeDirection() {
+    if (this.sortDirection=="arrow_upward")
+      this.sortDirection="arrow_downward"
+    else
+      this.sortDirection="arrow_upward"
+  }
+
+  showAdvanced() {
+    if (this.collapsed){
+      this.advancedSearchIcon="expand_less";
+      this.collapsed = !this.collapsed;
+    }
+    else {
+      this.advancedSearchIcon="tune";
+      this.collapsed = !this.collapsed;
+    }
+
+  }
+
+  ngOnInit(): void {
+    document.getElementById("searchFormField")?.focus();
+  }
 }
